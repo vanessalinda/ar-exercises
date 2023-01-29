e5ed6092587742253e580f240cd9fdd1b1d4e8b7
Vanessa Power <vanessa.l.power@gmail.com>
2023-01-28 21:49:43 -0800

    writing exercise 7

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,9 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+
+  validates_presence_of :first_name
+
+  validates_presence_of :store
+
+  validates_inclusion_of :hourly_rate, :in => 40..200
 end
