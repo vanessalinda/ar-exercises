2d87d63eabeb466dd299406a455487ef274c28e8
Vanessa Power <vanessa.l.power@gmail.com>
2023-01-28 20:21:54 -0800

    finish exercise 2

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,13 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+#Burnaby (annual_revenue of 300000, carries men's and women's apparel)
+burnaby = Store.create(name:'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel:true)
+
+# Richmond (annual_revenue of 1260000 carries women's apparel only)
+richmond = Store.create(name:'Richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel:true)
+
+# Gastown (annual_revenue of 190000 carries men's apparel only)
+gastown = Store.create(name: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+puts "Total number of stores: #{Store.count}"
--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,4 +5,10 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
-@store1
+@store1 = Store.find_by(id: 1)
+
+@store2 = Store.find_by(id: 2)
+
+@store1.update(name: 'New Westminster')
+
+#puts @store1.name
